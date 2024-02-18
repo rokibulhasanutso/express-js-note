da9cd3489189fd091f3dcf8f8e118c24bbb19392
rokibulhasan <rokibulhasanutsoo@gmail.com>
2024-02-18 11:02:43 +0600

    add express file download

--- 05_download-file/index.js
+++ 05_download-file/index.js
@@ -2,28 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000
 
-// object data
-const data = [
-	{
-		name: 'Rokibul Hasan',
-		city: 'Jamalpur',
-		occupation: 'Engineer'
-	},
-	{
-		name: 'Hridoy Hasan',
-		city: 'Tangail',
-		occupation: 'Doctor'
-	},
-	{
-		name: 'Humairah',
-		city: 'Shylet',
-		occupation: 'Student'
-	}
-]
-
-// send json response
+// send download file response
 app.get('/', (req, res) => {
-	res.json(data);
+	res.download('./image-file/flower-image.jpg');
 })
 
 // start server
